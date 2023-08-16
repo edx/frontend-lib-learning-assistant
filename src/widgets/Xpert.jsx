f46491bd776dbe863a84e43ff51f76a37f28e08f
michaelroytman <mroytman@edx.org>
2023-08-16 17:27:18 -0400

    fix: dispatch thunks correctly
    This commit fixes a bug where thunks were not being dispatched correctly. This is fixed by using the useDispatch hook to get a referenceto the dispatch function, which is then applied to the thunk.

--- src/widgets/Xpert.jsx
+++ src/widgets/Xpert.jsx
@@ -1,6 +1,6 @@
 import PropTypes from 'prop-types';
 import { useState, useEffect } from 'react';
-import { useSelector } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import ToggleXpert from '../components/ToggleXpertButton';
 import Sidebar from '../components/Sidebar';
 import { getChatResponse } from '../data/thunks';
@@ -8,12 +8,13 @@ import { getChatResponse } from '../data/thunks';
 const Xpert = ({ courseId }) => {
   const { messageList } = useSelector(state => state.learningAssistant);
   const [sidebarIsOpen, setSidebarIsOpen] = useState(false);
+  const dispatch = useDispatch();
 
   useEffect(() => {
     if (messageList[messageList.length - 1].role === 'user') {
-      getChatResponse(courseId);
+      dispatch(getChatResponse(courseId));
     }
-  }, [messageList, courseId]);
+  }, [dispatch, courseId, messageList]);
 
   return (
     <div>
