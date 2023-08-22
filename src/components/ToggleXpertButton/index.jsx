67d3d9bdebb9232e14bcc8f327a3647ab109cded
michaelroytman <mroytman@edx.org>
2023-08-22 14:34:16 -0400

    fix: change CSS button element selector to class selector
    This commit changes the CSS button element selector in the ToggleXpertButton to a CSS class selector (i.e. toggle). This is because the button selector was being applied in the frontend-app-learning application and causing changes to the page style.
    
    Eventually, it may be nice to use something like CSS modules or styled components to scope our CSS to this component, but, for now, this will do.

--- src/components/ToggleXpertButton/index.jsx
+++ src/components/ToggleXpertButton/index.jsx
@@ -18,7 +18,7 @@ const ToggleXpert = ({ isOpen, setIsOpen, courseId }) => {
   if (isOpen) {
     return (
       <button
-        className="open position-fixed d-flex flex-row bg-white p-2"
+        className="toggle open position-fixed d-flex flex-row bg-white p-2"
         data-testid="toggle-button"
         onClick={handleClick}
         type="button"
@@ -30,7 +30,7 @@ const ToggleXpert = ({ isOpen, setIsOpen, courseId }) => {
 
   return (
     <button
-      className="closed d-flex flex-column position-fixed justify-content-end align-items-end border-0"
+      className="toggle closed d-flex flex-column position-fixed justify-content-end align-items-end border-0"
       data-testid="toggle-button"
       onClick={handleClick}
       type="button"
