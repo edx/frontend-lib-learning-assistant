3aa7e21b5b5d17d20c8e13d72ea3e46635a200fe
Hunia Fatima <hunia.fatima@arbisoft.com>
2024-07-01 12:05:36 +0500

    perf: test commit

--- src/components/ChatBox/index.jsx
+++ src/components/ChatBox/index.jsx
@@ -11,7 +11,7 @@ const ChatBox = ({ chatboxContainerRef }) => {
   return (
     <div ref={chatboxContainerRef} className="flex-grow-1 scroller d-flex flex-column pb-4">
       {messageList.map(({ role, content, timestamp }) => (
-        <Message 
+        <Message
           key={timestamp.toString()}
           variant={role}
           message={content}
