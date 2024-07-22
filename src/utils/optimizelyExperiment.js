0ff4d4fe7785e7a3da8b01bc30d924869f5a04e5
Alison Langston <46360176+alangsto@users.noreply.github.com>
2024-07-22 16:18:29 -0400

    fix: modify optimizely tracking calls (#58)

--- src/utils/optimizelyExperiment.js
+++ src/utils/optimizelyExperiment.js
@@ -1,16 +1,8 @@
 import { getOptimizely } from '../data/optimizely';
 
-const trackChatBotLaunchOptimizely = (userId, userAttributes = {}) => {
-  const optimizelyInstance = getOptimizely();
-  optimizelyInstance.track('learning_assistant_chat_click', userId, userAttributes);
-};
-
 const trackChatBotMessageOptimizely = (userId, userAttributes = {}) => {
   const optimizelyInstance = getOptimizely();
   optimizelyInstance.track('learning_assistant_chat_message', userId, userAttributes);
 };
 
-export {
-  trackChatBotLaunchOptimizely,
-  trackChatBotMessageOptimizely,
-};
+export default trackChatBotMessageOptimizely;
