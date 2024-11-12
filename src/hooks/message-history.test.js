64983780bf4c46344481fcbdbe99a03c2041382d
Marcos <mrigoli@2u.com>
2024-11-12 16:06:56 -0300

    chore: Added coverage to useMessageHistory() hook

---
+++ src/hooks/message-history.test.js
@@ -0,0 +1,53 @@
+import { renderHook } from '@testing-library/react-hooks';
+
+import { useSelector } from 'react-redux';
+import { useMessageHistory } from './message-history';
+import { getLearningAssistantMessageHistory } from '../data/thunks';
+
+const mockDispatch = jest.fn();
+jest.mock('react-redux', () => ({
+  ...jest.requireActual('react-redux'),
+  useSelector: jest.fn(),
+  useDispatch: () => mockDispatch,
+}));
+
+jest.mock('../data/thunks');
+
+describe('Learning Assistant Message History Hooks', () => {
+  afterEach(() => {
+    jest.resetAllMocks();
+  });
+
+  describe('useMessageHistory()', () => {
+    let hook;
+    const fakeCourseId = 'course-v1:edx+test+23';
+
+    const renderTestHook = (courseId, isEnabled) => {
+      const mockedStoreState = { learningAssistant: { isEnabled } };
+      useSelector.mockImplementation(selector => selector(mockedStoreState));
+      hook = renderHook(() => useMessageHistory(courseId));
+      return hook;
+    };
+
+    it('should dispatch getLearningAssistantMessageHistory() with the chat history', () => {
+      renderTestHook(fakeCourseId, true);
+
+      expect(mockDispatch).toHaveBeenCalledTimes(1);
+      expect(getLearningAssistantMessageHistory).toHaveBeenCalledWith(fakeCourseId);
+    });
+
+    it('should NOT dispatch getLearningAssistantMessageHistory() when disabled', () => {
+      renderTestHook(fakeCourseId, false);
+
+      expect(mockDispatch).not.toHaveBeenCalled();
+      expect(getLearningAssistantMessageHistory).not.toHaveBeenCalled();
+    });
+
+    it('should NOT dispatch getLearningAssistantMessageHistory() with no courseId', () => {
+      renderTestHook(null, true);
+
+      expect(mockDispatch).not.toHaveBeenCalled();
+      expect(getLearningAssistantMessageHistory).not.toHaveBeenCalled();
+    });
+  });
+});
