d5019ab6416cccd00097c5e60e397a071b4d82a0
Varsha Menon <varshamenon04@gmail.com>
2024-11-15 13:39:34 -0500

    feat: add message divider between old and new messages

--- src/components/ChatBox/index.jsx
+++ src/components/ChatBox/index.jsx
@@ -3,15 +3,32 @@ import { useSelector } from 'react-redux';
 import PropTypes from 'prop-types';
 import Message from '../Message';
 import './ChatBox.scss';
+import MessageDivider from '../MessageDivider';
+
+function isToday(date) {
+  const today = new Date();
+  return (
+    date.getDate() === today.getDate()
+    && date.getMonth() === today.getMonth()
+    && date.getFullYear() === today.getFullYear()
+  );
+}
 
 // container for all of the messages
 const ChatBox = ({ chatboxContainerRef }) => {
   const { messageList, apiIsLoading } = useSelector(state => state.learningAssistant);
+  const messagesBeforeToday = messageList.filter((m) => (!isToday(new Date(m.timestamp))));
+  const messagesToday = messageList.filter((m) => (isToday(new Date(m.timestamp))));
 
+  // message divider should not display if no messages or if all messages sent today.
   return (
     <div ref={chatboxContainerRef} className="flex-grow-1 scroller d-flex flex-column pb-4">
-      {messageList.map(({ role, content, timestamp }) => (
-        <Message key={timestamp.toString()} variant={role} message={content} />
+      {messagesBeforeToday.map(({ role, content, timestamp }) => (
+        <Message key={timestamp} variant={role} message={content} />
+      ))}
+      {(messageList.length !== 0 && messagesBeforeToday.length !== 0) && (<MessageDivider text="Today" />)}
+      {messagesToday.map(({ role, content, timestamp }) => (
+        <Message key={timestamp} variant={role} message={content} />
       ))}
       {apiIsLoading && (
         <div className="loading">Xpert is thinking</div>
