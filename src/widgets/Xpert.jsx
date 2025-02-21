b51323cafbe376ec4f0f7340b17d6995666a6209
Varsha Menon <varshamenon04@gmail.com>
2025-02-21 15:59:21 -0500

    fix: off and control variation optimizely not working as expected

--- src/widgets/Xpert.jsx
+++ src/widgets/Xpert.jsx
@@ -1,7 +1,6 @@
 import PropTypes from 'prop-types';
 import { useEffect, useMemo } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { useModel } from '@src/generic/model-store'; // eslint-disable-line import/no-unresolved
 
 import { updateSidebarIsOpen, getLearningAssistantChatSummary } from '../data/thunks';
 import ToggleXpert from '../components/ToggleXpertButton';
@@ -10,6 +9,56 @@ import { OPTIMIZELY_AUDIT_TRIAL_LENGTH_EXPERIMENT_VARIATION_KEYS } from '../data
 import { ExperimentsProvider, useAuditTrialExperimentDecision } from '../experiments';
 import { CourseInfoProvider } from '../context';
 
+const XpertDisplay = ({
+  courseId,
+  contentToolsEnabled,
+  unitId,
+  isUpgradeEligible,
+  sidebarIsOpen,
+  setSidebarIsOpen,
+}) => {
+  const [decision] = useAuditTrialExperimentDecision();
+  const { enabled: experimentEnabled, variationKey } = decision || {};
+
+  // Any user should see xpert unless they are an audit learner not part of the experiment or part of the control.
+  const shouldDisplayXpert = (
+    !(variationKey === OPTIMIZELY_AUDIT_TRIAL_LENGTH_EXPERIMENT_VARIATION_KEYS.CONTROL && isUpgradeEligible)
+    && !(!experimentEnabled && isUpgradeEligible)
+  );
+
+  const XpertSidebar = (
+    <>
+      <ToggleXpert
+        courseId={courseId}
+        isOpen={sidebarIsOpen}
+        setIsOpen={setSidebarIsOpen}
+        contentToolsEnabled={contentToolsEnabled}
+      />
+      <Sidebar
+        courseId={courseId}
+        isOpen={sidebarIsOpen}
+        setIsOpen={setSidebarIsOpen}
+        unitId={unitId}
+      />
+    </>
+  );
+
+  return shouldDisplayXpert ? (
+    <ExperimentsProvider>
+      {XpertSidebar}
+    </ExperimentsProvider>
+  ) : null;
+};
+
+XpertDisplay.propTypes = {
+  courseId: PropTypes.string.isRequired,
+  contentToolsEnabled: PropTypes.bool.isRequired,
+  unitId: PropTypes.string.isRequired,
+  isUpgradeEligible: PropTypes.bool.isRequired,
+  sidebarIsOpen: PropTypes.func.isRequired,
+  setSidebarIsOpen: PropTypes.func.isRequired,
+};
+
 const Xpert = ({
   courseId,
   contentToolsEnabled,
@@ -17,10 +66,6 @@ const Xpert = ({
   isUpgradeEligible,
 }) => {
   const dispatch = useDispatch();
-
-  const {
-    isStaff,
-  } = useModel('courseHomeMeta', courseId);
 
   useEffect(() => {
     dispatch(getLearningAssistantChatSummary(courseId));
@@ -40,36 +85,17 @@ const Xpert = ({
     dispatch(updateSidebarIsOpen(isOpen));
   };
 
-  const [decision] = useAuditTrialExperimentDecision();
-  const { enabled: experimentEnabled, variationKey } = decision || {};
-
-  // if a user is not part of the experiment, not part of the control, or not eligible for upgrade, they should
-  // still see xpert
-  const shouldDisplayXpert = (
-    !experimentEnabled
-    || !(variationKey === OPTIMIZELY_AUDIT_TRIAL_LENGTH_EXPERIMENT_VARIATION_KEYS.CONTROL)
-    || isStaff
-  );
-
   return isEnabled ? (
     <CourseInfoProvider value={courseInfo}>
       <ExperimentsProvider>
-        { shouldDisplayXpert ? (
-          <>
-            <ToggleXpert
-              courseId={courseId}
-              isOpen={sidebarIsOpen}
-              setIsOpen={setSidebarIsOpen}
-              contentToolsEnabled={contentToolsEnabled}
-            />
-            <Sidebar
-              courseId={courseId}
-              isOpen={sidebarIsOpen}
-              setIsOpen={setSidebarIsOpen}
-              unitId={unitId}
-            />
-          </>
-        ) : null }
+        <XpertDisplay
+          courseId={courseId}
+          contentToolsEnabled={contentToolsEnabled}
+          unitId={unitId}
+          isUpgradeEligible={isUpgradeEligible}
+          sidebarIsOpen={sidebarIsOpen}
+          setSidebarIsOpen={setSidebarIsOpen}
+        />
       </ExperimentsProvider>
     </CourseInfoProvider>
   ) : null;
